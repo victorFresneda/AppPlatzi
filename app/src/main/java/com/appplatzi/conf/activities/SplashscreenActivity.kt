0c48305e0c67449e6c3ab259760340b85897feda
victorFresneda <vafresnedah@hotmail.com>
2020-08-19 02:23:18 -0500

    All fragments are added

---
+++ app/src/main/java/com/appplatzi/conf/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.appplatzi.conf.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appplatzi.conf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
