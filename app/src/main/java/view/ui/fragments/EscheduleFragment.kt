c63ffead12776827201f7b214d4c9688938fce61
victorFresneda <vafresnedah@hotmail.com>
2020-09-21 23:55:17 -0500

    I create componentes of Schedule fragment

--- app/src/main/java/view/ui/fragments/EscheduleFragment.kt
+++ app/src/main/java/view/ui/fragments/EscheduleFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.appplatzi.conf.R
+import kotlinx.android.synthetic.main.fragment_eschedule.*
+import view.adapter.ScheduleAdapter
+import viewmodel.ScheduleViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class EscheduleFragment : Fragment() {
+
+    private lateinit var schededuleAdapter: ScheduleAdapter
+    private lateinit var viewModel: ScheduleViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +29,20 @@ class EscheduleFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_eschedule, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(ScheduleViewModel::class.java)
+        viewModel.refresh()
+
+        schededuleAdapter = ScheduleAdapter(this)
+
+        rvSchedule.apply {
+            layoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL. false)
+
+        }
+    }
+
 
 }
--- app/src/main/java/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/viewmodel/ScheduleViewModel.kt
@@ -2,11 +2,12 @@ package viewmodel
 
 import android.telecom.Conference
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.appplatzi.conf.network.Callback
 import com.appplatzi.conf.network.FirestoreSevice
 import java.lang.Exception
 
-class ScheduleViewModel {
+class ScheduleViewModel: ViewModel(){
     val firestoreService = FirestoreSevice()
     var listSchedule:MutableLiveData<List<Conference>> = MutableLiveData()
     var isLoading = MutableLiveData<Boolean>()
