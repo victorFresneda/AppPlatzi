769f45181cde7a4a34f3f4b72596295a5339f88b
victorFresneda <vafresnedah@hotmail.com>
2020-10-04 00:12:17 -0500

    Application runnig

--- app/src/main/java/com/appplatzi/conf/activities/MainActivity.kt
+++ app/src/main/java/com/appplatzi/conf/activities/MainActivity.kt
@@ -2,17 +2,23 @@ package com.appplatzi.conf.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.appplatzi.conf.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-
+        setActionBar(findViewById(R.id.toolbarMain))
+        configNav()
         }
+
+    fun configNav(){
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragContent))
     }
+    }
+
+
