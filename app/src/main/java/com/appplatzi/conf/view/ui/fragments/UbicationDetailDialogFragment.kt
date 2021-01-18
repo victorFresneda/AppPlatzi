7f045f70b1c28de0c7331c808a8bf2559b38b6e2
victorFresneda <vafresnedah@hotmail.com>
2021-01-18 12:46:14 -0500

    I customize the map

--- app/src/main/java/com/appplatzi/conf/view/ui/fragments/UbicationDetailDialogFragment.kt
+++ app/src/main/java/com/appplatzi/conf/view/ui/fragments/UbicationDetailDialogFragment.kt
@@ -1,24 +1,30 @@
 package com.appplatzi.conf.view.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import com.appplatzi.conf.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class UbicationDetailDialogFragment : Fragment() {
+class UbicationDetailDialogFragment : DialogFragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? { return inflater.inflate(R.layout.fragment_ubication_detail_dialog, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
